a15d628c6ef59c4006a0eeb270398eec7c61aae7
LuizFelipeConquesta <44447690+FehConquesta@users.noreply.github.com>
2024-03-08 11:49:11 -0300

    Metodo getLivroById

--- src/main/java/br/com/bookshelf/Bookshelf/controller/LivroController.java
+++ src/main/java/br/com/bookshelf/Bookshelf/controller/LivroController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bookshelf.model.Livro;
 
